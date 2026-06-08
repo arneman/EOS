84d02a31639da560db036224f2530e79b7b2e2ac
Arne <7462507+arneman@users.noreply.github.com>
2026-06-08 09:01:31 +0200

    also start mqtt bridge

--- fix_eos.sh
+++ fix_eos.sh
@@ -2,3 +2,5 @@ systemctl stop eos
 rm -rf ~/.local/share/net.akkudoktor.eos_broken
 mv -f ~/.local/share/net.akkudoktor.eos ~/.local/share/net.akkudoktor.eos_broken
 systemctl start eos
+sleep 5
+systemctl start mqtt_eos_bridge
