fb39c14e199c9d21ae765c3a8022824cb9191f9a
Arne <7462507+arneman@users.noreply.github.com>
2026-06-10 08:22:44 +0200

    change paths

--- fix_eos.sh
+++ fix_eos.sh
@@ -1,6 +1,6 @@
 systemctl stop eos
-rm -rf ~/.local/share/net.akkudoktor.eos_broken
-mv -f ~/.local/share/net.akkudoktor.eos ~/.local/share/net.akkudoktor.eos_broken
+rm -rf root/.local/share/net.akkudoktor.eos_broken
+mv -f root/.local/share/net.akkudoktor.eos root/.local/share/net.akkudoktor.eos_broken
 systemctl start eos
 sleep 5
 systemctl start mqtt_eos_bridge
